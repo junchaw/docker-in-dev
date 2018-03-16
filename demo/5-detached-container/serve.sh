f14dd8342c5e4b088e6f8aa935ed4c77dd860389
wujc <wujc@dxy.cn>
2018-03-16 17:40:29 +0800

    stop.sh

--- demo/5-detached-container/serve.sh
+++ demo/5-detached-container/serve.sh
@@ -2,17 +2,7 @@
 
 pwd="$( cd "$( dirname "$0" )" && pwd )"
 
-if [[ $(docker container ls --format="{{.Names}}" | grep -e '^localhost$') ]]; then
-  echo -e "Container with name 'localhost' existed, stopping..."
-  docker container stop localhost
-  echo -e "Stopped\n"
-fi
-
-if [[ $(docker container ls -a --format="{{.Names}}" | grep -e '^localhost$') ]]; then
-  echo -e "Container with name 'localhost' existed, removing..."
-  docker container rm localhost
-  echo -e "Removed\n"
-fi
+${pwd}/stop.sh
 
 docker run -dit \
   --name localhost \
